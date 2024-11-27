bfc46f84295b88e4c16a1c92c9461b3e08cd1142
Brunno Asquino <brunnof.asquino@gmail.com>
2024-11-27 20:49:57 -0300

    Atualizações - Aula JS

--- AsquinnoWebsite/Aulas_page/AulasJS.js
+++ AsquinnoWebsite/Aulas_page/AulasJS.js
@@ -1,24 +1,100 @@
-// AULA
-function toggleColapse_aula() {
-    const conteudo1 = document.getElementById('conteudo1');
-    const style1 = document.getElementById('btnColapse1');
-    const icon1 = document.getElementById('icon1');
+// NAVEGAÇÃO
+const cordas = document.getElementById("cordas");
+const cordasNav = document.getElementById("cordasNav");
+const percussao = document.getElementById("percussao");
+const percussaoNav = document.getElementById("percussaoNav");
+const sopro = document.getElementById("sopro");
+const soproNav = document.getElementById("soproNav");
+const vocal = document.getElementById("vocal");
+const vocalNav = document.getElementById("vocalNav");
+const teclas = document.getElementById("teclas");
+const teclasNav = document.getElementById("teclasNav");
+const emGrupo = document.getElementById("emGrupo");
+const emGrupoNav = document.getElementById("emGrupoNav");
 
-    if (conteudo1.style.display === 'none' || conteudo1.style.display === ''){
-        conteudo1.style.display = 'block';
-        style1.style.background = 'linear-gradient(to right, #8900A0, #D100F3)';
-        icon1.style.transform = 'rotate(180deg)';
+cordas.addEventListener("click", () => {
+    cordasNav.style.display = "flex";
+    percussaoNav.style.display = "none";
+    soproNav.style.display = "none";
+    vocalNav.style.display = "none";
+    teclasNav.style.display = "none";
+    emGrupoNav.style.display = "none";
+});
+percussao.addEventListener("click", () => {
+    cordasNav.style.display = "none";
+    percussaoNav.style.display = "flex";
+    soproNav.style.display = "none";
+    vocalNav.style.display = "none";
+    teclasNav.style.display = "none";
+    emGrupoNav.style.display = "none";
+});
+sopro.addEventListener("click", () => {
+    cordasNav.style.display = "none";
+    percussaoNav.style.display = "none";
+    soproNav.style.display = "flex";
+    vocalNav.style.display = "none";
+    teclasNav.style.display = "none";
+    emGrupoNav.style.display = "none";
+});
+vocal.addEventListener("click", () => {
+    cordasNav.style.display = "none";
+    percussaoNav.style.display = "none";
+    soproNav.style.display = "none";
+    vocalNav.style.display = "flex";
+    teclasNav.style.display = "none";
+    emGrupoNav.style.display = "none";
+});
+teclas.addEventListener("click", () => {
+    cordasNav.style.display = "none";
+    percussaoNav.style.display = "none";
+    soproNav.style.display = "none";
+    vocalNav.style.display = "none";
+    teclasNav.style.display = "flex";
+    emGrupoNav.style.display = "none";
+});
+emGrupo.addEventListener("click", () => {
+    cordasNav.style.display = "none";
+    percussaoNav.style.display = "none";
+    soproNav.style.display = "none";
+    vocalNav.style.display = "none";
+    teclasNav.style.display = "none";
+    emGrupoNav.style.display = "flex";
+});
 
-        conteudo2.style.display = 'none';
-        style2.style.background = 'linear-gradient(to right, #232323, #434343)';
-        icon2.style.transform = 'rotate(0deg)';
+// CONTEÚDO
+const topic1 = document.getElementById("topic1");
+const content1 = document.getElementById("content1");
+const topic2 = document.getElementById("topic2");
+const content2 = document.getElementById("content2");
+const topic3 = document.getElementById("topic3");
+const content3 = document.getElementById("content3");
+const topic4 = document.getElementById("topic4");
+const content4 = document.getElementById("content4");
 
-        conteudo3.style.display = 'none';
-        style3.style.background = 'linear-gradient(to right, #232323, #434343)';
-        icon3.style.transform = 'rotate(0deg)';
-    } else {
-        conteudo1.style.display = 'none';
-        style1.style.background = 'linear-gradient(to right, #232323, #434343)';
-        icon1.style.transform = 'rotate(0deg)';
-    }
-}
+topic1.addEventListener("click", () => {
+    content1.style.display = "block";
+    content2.style.display = "none";
+    content3.style.display = "none";
+    content4.style.display = "none";
+});
+
+topic2.addEventListener("click", () => {
+    content1.style.display = "none";
+    content2.style.display = "block";
+    content3.style.display = "none";
+    content4.style.display = "none";
+});
+
+topic3.addEventListener("click", () => {
+    content1.style.display = "none";
+    content2.style.display = "none";
+    content3.style.display = "block";
+    content4.style.display = "none";
+});
+
+topic4.addEventListener("click", () => {
+    content1.style.display = "none";
+    content2.style.display = "none";
+    content3.style.display = "none";
+    content4.style.display = "block";
+});
