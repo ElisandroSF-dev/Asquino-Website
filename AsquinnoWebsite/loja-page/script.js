fdd0395a0c2808aad60a1f448fd6d2a4885c09ae
Yan <yancarlosfarias1998@gmail.com>
2024-11-30 16:42:08 -0300

    d

--- AsquinnoWebsite/loja-page/script.js
+++ AsquinnoWebsite/loja-page/script.js
@@ -6,3 +6,14 @@ clearButton.addEventListener('click',  
   searchInput.value   
  = '';
 });
+
+const menuItems = document.querySelectorAll('.category-nav li');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Remove a classe 'active' de todos os itens
+    menuItems.forEach(item => item.classList.remove('active'));
+    // Adiciona a classe 'active' 1  ao item clicado
+    item.classList.add('active');
+  });
+});
